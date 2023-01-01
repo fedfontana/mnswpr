db6981216851d3b74d57af9a18020bc8897094b9
fedfontana <federico.fontana2000@gmail.com>
2023-01-01 23:02:12 +0100

    show mine and flag count. Do not allow flagging when #flags >= #mines

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 #![allow(dead_code, unused)]
 
 use std::fmt::Display;
-use std::io::{Write, stdout, stdin};
+use std::io::{stdin, stdout, Write};
 
 use rand::{seq::SliceRandom, thread_rng, Rng};
 
-use termion::event::{Key, Event, MouseEvent};
-use termion::input::{TermRead, MouseTerminal};
-use termion::raw::IntoRawMode;
 use termion::color;
+use termion::event::{Event, Key, MouseEvent};
+use termion::input::{MouseTerminal, TermRead};
+use termion::raw::IntoRawMode;
 
 #[derive(Copy, Clone, Debug)]
 pub enum CellState {
@@ -41,16 +41,16 @@ impl Default for CellContent {
 }
 
 const NBOR_COUNT_TO_FG_COLOR: [color::Fg<&'static dyn color::Color>; 9] = [
-                                            color::Fg(&color::Reset),       // 0
-                                            color::Fg(&color::LightBlue),   // 1
-                                            color::Fg(&color::LightBlue),   // 2
-                                            color::Fg(&color::Blue),        // 3
-                                            color::Fg(&color::Blue),        // 4
-                                            color::Fg(&color::LightRed),    // 5
-                                            color::Fg(&color::LightRed),    // 6
-                                            color::Fg(&color::Red),         // 7
-                                            color::Fg(&color::Red),         // 8
-                                            ];
+    color::Fg(&color::Reset),     // 0
+    color::Fg(&color::LightBlue), // 1
+    color::Fg(&color::LightBlue), // 2
+    color::Fg(&color::Blue),      // 3
+    color::Fg(&color::Blue),      // 4
+    color::Fg(&color::LightRed),  // 5
+    color::Fg(&color::LightRed),  // 6
+    color::Fg(&color::Red),       // 7
+    color::Fg(&color::Red),       // 8
+];
 
 const BG_COLOR: color::Bg<color::Rgb> = color::Bg(color::Rgb(40, 40, 40));
 
@@ -65,17 +65,23 @@ impl Display for Cell {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self.state {
             CellState::Open => match self.content {
-                CellContent::Mine => write!(f, "{}*{}", color::Bg(color::Red), color::Bg(color::Reset)),
-                CellContent::Empty => write!(f, "{}{}{}{}{}", 
-                    BG_COLOR, 
-                    NBOR_COUNT_TO_FG_COLOR[self.neighboring_bomb_count], 
-                    self.neighboring_bomb_count, 
-                    color::Fg(color::Reset), 
+                CellContent::Mine => {
+                    write!(f, "{}*{}", color::Bg(color::Red), color::Bg(color::Reset))
+                }
+                CellContent::Empty => write!(
+                    f,
+                    "{}{}{}{}{}",
+                    BG_COLOR,
+                    NBOR_COUNT_TO_FG_COLOR[self.neighboring_bomb_count],
+                    self.neighboring_bomb_count,
+                    color::Fg(color::Reset),
                     color::Bg(color::Reset)
                 ),
             },
             CellState::Closed => write!(f, "."),
-            CellState::Flagged => write!(f, "{}F{}", color::Bg(color::Blue), color::Bg(color::Reset)),
+            CellState::Flagged => {
+                write!(f, "{}F{}", color::Bg(color::Blue), color::Bg(color::Reset))
+            }
         }
     }
 }
@@ -92,6 +98,8 @@ struct Field {
     grid: Vec<Cell>,
     cursor: Cursor,
     uncovered_empty_cells: usize,
+    mine_count: usize,
+    flag_count: usize,
 }
 
 impl Field {
@@ -102,11 +110,13 @@ impl Field {
 
         let mut grid = Vec::with_capacity(rows * cols);
         let mut uncovered_empty_cells = 0;
+        let mut mine_count = 0;
 
         // Generate random board
         let mut rng = thread_rng();
         for idx in 0..rows * cols {
             let cell_content = if rng.gen_range(1..=100) <= bomb_percentage {
+                mine_count += 1;
                 CellContent::Mine
             } else {
                 uncovered_empty_cells += 1;
@@ -134,8 +144,8 @@ impl Field {
                         continue;
                     }
 
-                    let current_row  = row+delta_row;
-                    let current_col = col+delta_col;
+                    let current_row = row + delta_row;
+                    let current_col = col + delta_col;
                     // Do not consider out of bounds cells
                     if current_row >= rows as isize
                         || current_col >= cols as isize
@@ -145,18 +155,24 @@ impl Field {
                         continue;
                     }
 
-                    match grid[current_row as usize * cols + current_col as usize]
-                        .content
-                    {
+                    match grid[current_row as usize * cols + current_col as usize].content {
                         CellContent::Mine => count += 1,
                         CellContent::Empty => {}
                     }
                 }
             }
-            grid[row as usize*cols+col as usize].neighboring_bomb_count = count;
+            grid[row as usize * cols + col as usize].neighboring_bomb_count = count;
         }
 
-        Self { rows, cols, grid, cursor: Cursor { row: 0, col: 0 }, uncovered_empty_cells}
+        Self {
+            rows,
+            cols,
+            grid,
+            cursor: Cursor { row: 0, col: 0 },
+            uncovered_empty_cells,
+            mine_count,
+            flag_count: 0,
+        }
     }
 
     fn get(&self, row: usize, col: usize) -> Option<Cell> {
@@ -192,11 +208,10 @@ impl Field {
             match cell_under_cursor.content {
                 CellContent::Mine => {
                     // Do nothing, the caller will do stuff with the return value
-                },
+                }
                 CellContent::Empty => {
-                    //TODO open neighbors if they have 0 bombs?
                     self.uncovered_empty_cells -= 1;
-                },
+                }
             }
         }
 
@@ -204,16 +219,27 @@ impl Field {
     }
 
     fn toggle_flag_at_cursor(&mut self) {
+        let can_flag = self.flag_count < self.mine_count;
         let cell_under_cursor = self.get_mut(self.cursor.row, self.cursor.col).unwrap();
         match cell_under_cursor.state {
-            CellState::Open => {},
-            CellState::Closed =>  cell_under_cursor.set_state(CellState::Flagged),
-            CellState::Flagged => cell_under_cursor.set_state(CellState::Closed),
+            CellState::Open => {}
+            CellState::Closed => {
+                if can_flag {
+                    cell_under_cursor.set_state(CellState::Flagged);
+                    self.flag_count += 1;
+                }
+            },
+            CellState::Flagged => {
+                cell_under_cursor.set_state(CellState::Closed);
+                self.flag_count -= 1;
+            },
         };
     }
 
     fn uncover_all(&mut self) {
-        self.grid.iter_mut().for_each(|cell| cell.state = CellState::Open);
+        self.grid
+            .iter_mut()
+            .for_each(|cell| cell.state = CellState::Open);
     }
 }
 
@@ -237,7 +263,7 @@ impl Display for Field {
     }
 }
 
-//TODO fix the board continously going down with time
+//TODO fix the board continously going down with each subsequent move
 //TODO add functionality to uncover the whole set of 0s when clicking on one of them
 //TODO add mouse click support (supported by termion)
 
@@ -249,21 +275,26 @@ impl Display for Field {
 //  - auto-counter or something like that. When this option is active, numbers that have too many flags around them turn bright red,
 //          and the ones with the right amount of flags around them get printed green
 
-//TODO add a retry option
+//TODO add a retry option after concluding a match
 //TODO generate board when clicking on first cell. Either generate a number or a whole area of numbers under the cursor in a way that the first tile cannot be a bomb.
 
 //TODO fix the colors not covering the whole board
 //TODO add more different colors for each bomb count
 //TODO when a match is lost, highlight the flags in the wrong place with a different color
-//TODO print the number of the remaining bombs
-//TODO do not allow flagging when current_flag_count > bomb_count
 fn main() {
     let mut field = Field::new(10, 10, 20);
 
     let stdin = stdin();
     let mut stdout = stdout().into_raw_mode().unwrap();
 
-    write!(stdout, "{}{}{field}",termion::clear::All, termion::cursor::Goto(1,1 ));
+    write!(
+        stdout,
+        "{}{}Mines:{}    Flags:{}\r\n{field}",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1),
+        field.mine_count,
+        field.flag_count
+    );
     stdout.flush().unwrap();
 
     for c in stdin.events() {
@@ -274,47 +305,67 @@ fn main() {
                     if field.cursor.row > 0 {
                         field.cursor.row -= 1;
                     }
-                },
-                Key::Char('a') | Key::Char('A') |  Key::Left => {
+                }
+                Key::Char('a') | Key::Char('A') | Key::Left => {
                     if field.cursor.col > 0 {
                         field.cursor.col -= 1;
                     }
-                },
+                }
                 Key::Char('s') | Key::Char('S') | Key::Down => {
-                    if field.cursor.row < field.rows-1 {
+                    if field.cursor.row < field.rows - 1 {
                         field.cursor.row += 1;
                     }
-                },
+                }
                 Key::Char('d') | Key::Char('D') | Key::Right => {
-                    if field.cursor.col < field.cols-1{
+                    if field.cursor.col < field.cols - 1 {
                         field.cursor.col += 1;
                     }
-                },
-                Key::Char(' ') => {
-                    match field.uncover_at_cursor() {
-                        CellContent::Mine => {
-                            let cell = field.get(field.cursor.row, field.cursor.col).unwrap();
-                            if !matches!(cell.state, CellState::Flagged) { 
-                                field.uncover_all();
-                                write!(stdout, "{}{}{field}",termion::clear::All, termion::cursor::Goto(1,1));
-                                println!("{}You lost!{}", color::Fg(color::LightRed), color::Fg(color::Reset));
-                                break;
-                            }
-                        },
-                        CellContent::Empty => {},
+                }
+                Key::Char(' ') => match field.uncover_at_cursor() {
+                    CellContent::Mine => {
+                        let cell = field.get(field.cursor.row, field.cursor.col).unwrap();
+                        if !matches!(cell.state, CellState::Flagged) {
+                            field.uncover_all();
+                            write!(
+                                stdout,
+                                "{}{}Mines:{}    Flags:{}\r\n{field}",
+                                termion::clear::All,
+                                termion::cursor::Goto(1, 1),
+                                field.mine_count,
+                                field.flag_count
+                            );
+                            println!(
+                                "{}You lost!{}",
+                                color::Fg(color::LightRed),
+                                color::Fg(color::Reset)
+                            );
+                            break;
+                        }
                     }
+                    CellContent::Empty => {}
                 },
                 Key::Char('f') | Key::Char('F') => field.toggle_flag_at_cursor(),
-                _ => {},
+                _ => {}
             }
         }
-        write!(stdout, "{}{field}",termion::clear::All);
+        write!(
+            stdout,
+            "{}{}Mines:{}    Flags:{}\r\n{field}",
+            termion::clear::All,
+            termion::cursor::Goto(1, 1),
+            field.mine_count,
+            field.flag_count
+        );
         if field.uncovered_empty_cells == 0 {
-            write!(stdout, "{}You won!{}",color::Fg(color::Green), color::Fg(color::Reset));
+            write!(
+                stdout,
+                "{}You won!{}",
+                color::Fg(color::Green),
+                color::Fg(color::Reset)
+            );
             stdout.flush().unwrap();
             break;
         }
         stdout.flush().unwrap();
-
     }
 }

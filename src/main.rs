f19dfbbeda584b64ec96973603c52307b1839eb9
fedfontana <federico.fontana2000@gmail.com>
2023-01-02 16:55:52 +0100

    removed todo

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,6 @@ use crate::game::Minesweeper;
 //          and the ones with the right amount of flags around them get printed green
 
 //TODO add a retry option after concluding a match
-//TODO when a match is lost, highlight the flags in the wrong place with a different color
 //TODO add bg color for uncovered cells (the original gray (185, 185, 185))
 //TODO generate board when clicking on first cell. 
 //  - Either generate a number or a whole area of numbers under the cursor in a way that the first tile cannot be a bomb.

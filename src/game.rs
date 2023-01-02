a8285171c91229b15e1a89b34a3ae50295b39d3d
fedfontana <federico.fontana2000@gmail.com>
2023-01-02 16:55:28 +0100

    somewhat abstracted printing the board

--- src/game.rs
+++ src/game.rs
@@ -59,10 +59,31 @@ impl Minesweeper {
         write!(f, "{}", str_repr);
     }
 
-    pub fn print_field_game_lost(&self, f: &mut impl Write) {
-        self.print_game_state(f);
-        write!(f, "\r{}You lost{FG_RESET}", color::Fg(color::Red));
-        f.flush().unwrap();
+    pub fn print_field_game_lost(&self, f: &mut impl Write, palette: &Palette) {
+        let mut str_repr = String::new(); //TODO use with_capacity()? / implement this in a more efficient way
+
+        for row in 0..self.rows {
+            for col in 0..self.cols {
+                let cell = self.field.get(row, col).unwrap();
+
+                let sep = if self.cursor.row == row && self.cursor.col == col {
+                    ('[', ']')
+                } else {
+                    (' ', ' ')
+                };
+
+                str_repr = format!(
+                    "{str_repr}{bg}{}{cell_repr}{bg}{}{BG_RESET}",
+                    sep.0,
+                    sep.1,
+                    bg = palette.bg,
+                    cell_repr = cell.to_string_with_palette_lost(palette),
+                );
+            }
+            str_repr = format!("{str_repr}\r\n");
+        }
+
+        write!(f, "{}", str_repr);
     }
 
     /// Prints the game state. Prints bomb count and flag count and appends the output of `self.print_field` to it
@@ -75,6 +96,24 @@ impl Minesweeper {
             self.field.flag_count
         );
         self.print_field(f, &DEFAULT_PALETTE);
+        f.flush().unwrap();
+    }
+
+    pub fn lose_screen(&self, f: &mut impl Write) {
+        write!(
+            f,
+            "{}Mines:{}    Flags:{}\r\n",
+            termion::cursor::Goto(1, 1),
+            self.field.mine_count,
+            self.field.flag_count
+        );
+        self.print_field_game_lost(f, &DEFAULT_PALETTE);
+        write!(
+            f,
+            "{}You lost!{}\r\n",
+            color::Fg(color::LightRed),
+            color::Fg(color::Reset)
+        );
         f.flush().unwrap();
     }
 }

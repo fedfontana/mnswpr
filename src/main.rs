5262c02783a18f461d37ad93fd94bae82a8dd1ab
fedfontana <federico.fontana2000@gmail.com>
2023-01-02 12:58:56 +0100

    better colors

--- src/main.rs
+++ src/main.rs
@@ -42,18 +42,18 @@ impl Default for CellContent {
 }
 
 const NBOR_COUNT_TO_FG_COLOR: [color::Fg<&'static dyn color::Color>; 9] = [
-    color::Fg(&color::Reset),     // 0
-    color::Fg(&color::LightBlue), // 1
-    color::Fg(&color::LightBlue), // 2
-    color::Fg(&color::Blue),      // 3
-    color::Fg(&color::Blue),      // 4
-    color::Fg(&color::LightRed),  // 5
-    color::Fg(&color::LightRed),  // 6
-    color::Fg(&color::Red),       // 7
-    color::Fg(&color::Red),       // 8
+    color::Fg(&color::Reset),               // 0
+    color::Fg(&color::Rgb(70, 70, 255)),      // 1 // original is Rgb(0, 0, 255)
+    color::Fg(&color::Rgb(0, 130, 0)),      // 2
+    color::Fg(&color::Rgb(200, 0, 0)),      // 3
+    color::Fg(&color::Rgb(50, 50, 131)),      // 4 // original is Rgb(0, 0, 131)
+    color::Fg(&color::Rgb(132, 0 ,1)),      // 5
+    color::Fg(&color::Rgb(0, 130, 132)),    // 6
+    color::Fg(&color::Rgb(132, 0, 132)),    // 7
+    color::Fg(&color::Rgb(117, 117, 117)),  // 8
 ];
 
-const BG_COLOR: color::Bg<color::Rgb> = color::Bg(color::Rgb(40, 40, 40));
+const BG_COLOR: color::Bg<color::Rgb> = color::Bg(color::Rgb(30, 30, 30));
 
 #[derive(Copy, Clone, Debug, Default)]
 pub struct Cell {
